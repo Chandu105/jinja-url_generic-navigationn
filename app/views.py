cf45921d700da95623c0a74ee825c7545ed64063
Chandu105 <bandaruchandana2001@gmail.com>
2023-11-20 22:48:28 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+def chandana(request):
+    return render(request,'chandana.html')
+def mounika(request):
+    return render(request,'mounika.html')
